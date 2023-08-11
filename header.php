fd3c7773f727911af12bcc7ac270456aceb9a2a8
yuseon <yyizi0913@gmail.com>
2023-08-11 14:27:08 +0900

    product_slide

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
                 </fieldset>
             </form>
             <a href="./login.html" id="login"><img src="./images/icon_person.png" alt="로그인"></a>
-            <a href="#" id="cart"><img src="./images/icon_cart.png" alt="장바구니"></a>
+            <a href="./review.html" id="cart"><img src="./images/icon_cart.png" alt="장바구니"></a>
             <a href="#" id="menu"><img src="./images/icon_menu.png" alt="메뉴"></a>
         </div>
         <!-- menu 클릭 시 open nav -->
@@ -35,13 +35,13 @@
             </div>
             <!-- 내비게이션 -->
             <nav>
-                <a href="#">선글라스</a>
-                <a href="#">안경</a>
-                <a href="#">콜라보레이션</a>
-                <a href="#">스타일</a>
-                <a href="#">브랜드이슈</a>
+                <a href="./product_list.html">선글라스</a>
+                <a href="./product_list.html">안경</a>
+                <a href="./product_list.html">콜라보레이션</a>
+                <a href="./product_list.html">스타일</a>
+                <a href="./product_list.html">브랜드이슈</a>
                 <a href="#">매장보기</a>
-                <a href="#">수리서비스</a>
+                <a href="./product_list.html">수리서비스</a>
             </nav>
         </div>
     </header>
